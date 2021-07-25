8e304f0e9a1b4f84ee9f838a13560fc1af46b382
João Henrique Moretti Pellissari <pellissari.joao@gmail.com>
2021-07-25 13:51:44 -0300

    test: ensure garden has at least one irrigable patch

--- src/domain/garden.spec.ts
+++ src/domain/garden.spec.ts
@@ -1,39 +1,49 @@
 import { left } from '../core/either'
 import { InvalidSizeError } from './errors/invalid-size-error'
-import { Garden } from './garden'
-import { sizeType } from './size'
+import { MissingIrrigablePatchError } from './errors/missing-irrigable-patch-error'
+import { createGardenDTO, Garden } from './garden'
+
+const makeFakeCreateGardenDTO = (width = 2, height = 2): createGardenDTO => ({
+  size: {
+    width,
+    height
+  },
+  irrigablePatches: [
+    { x: 0, y: 0 }
+  ]
+})
 
 describe('Garden Entiy', () => {
   test('should not create Garden with invalid width size', () => {
-    const size: sizeType = {
-      width: 0,
-      height: 1
-    }
-    const gardenOrError = Garden.create(size)
+    const gardenOrError = Garden.create(makeFakeCreateGardenDTO(0, 1))
 
     expect(gardenOrError).toEqual(left(new InvalidSizeError()))
   })
 
   test('should not create Garden with invalid height size', () => {
-    const size: sizeType = {
-      width: 1,
-      height: 0
-    }
-    const gardenOrError = Garden.create(size)
+    const gardenOrError = Garden.create(makeFakeCreateGardenDTO(1, 0))
 
     expect(gardenOrError).toEqual(left(new InvalidSizeError()))
   })
 
+  test('should not create Garden without at least one irrigable patch', () => {
+    const gardenOrError = Garden.create({
+      ...makeFakeCreateGardenDTO(),
+      irrigablePatches: [{ x: 10, y: 10 }]
+    })
+
+    expect(gardenOrError.isLeft()).toBeTruthy()
+    expect(gardenOrError.value).toEqual(new MissingIrrigablePatchError())
+  })
+
   test('should create Garden with patches', () => {
-    const size: sizeType = {
-      width: 4,
-      height: 4
-    }
-    const gardenOrError = Garden.create(size)
+    const gardenOrError = Garden.create(makeFakeCreateGardenDTO())
     const garden = gardenOrError.isRight() ? gardenOrError.value : null
 
     expect(gardenOrError.isRight()).toBeTruthy()
-    expect(garden.size).toEqual(size)
-    expect(garden.patches.length).toEqual(size.width * size.height)
+    expect(garden.size).toEqual(makeFakeCreateGardenDTO().size)
+    expect(garden.patches.length).toEqual(
+      makeFakeCreateGardenDTO().size.width * makeFakeCreateGardenDTO().size.height
+    )
   })
 })
