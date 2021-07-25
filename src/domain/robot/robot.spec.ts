53313a8fdec94d5231409a39b4e76e14b4354e95
João Henrique Moretti Pellissari <pellissari.joao@gmail.com>
2021-07-25 18:40:31 -0300

    test: ensure robot can not be moved to north if out of bounds

--- src/domain/robot/robot.spec.ts
+++ src/domain/robot/robot.spec.ts
@@ -48,4 +48,21 @@ describe('Robot Entity', () => {
 
     expect(robot.heading).toEqual('N');
   });
+
+  test('should not move robot one position to north if out of bounds', () => {
+    const garden = Garden.create({
+      size: { width: 2, height: 2 },
+      irrigablePatches: [{ x: 0, y: 0 }],
+    }).value as Garden;
+
+    const robotOrError = Robot.create(garden);
+    const robot = robotOrError.isRight() ? robotOrError.value : null;
+
+    robot.move();
+    const moveOrError = robot.move();
+
+    expect(robot.heading).toEqual('N');
+    expect(moveOrError.isLeft()).toBeTruthy();
+    expect(robot.movements).toEqual(['N']);
+  });
 });
