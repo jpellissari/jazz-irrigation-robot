3a66176ebc6780cab49afb55ce68966ffb569bf8
João Henrique Moretti Pellissari <pellissari.joao@gmail.com>
2021-07-25 18:14:19 -0300

    feat: ensure robot can be created with default values

--- src/domain/robot/robot.ts
+++ src/domain/robot/robot.ts
@@ -1,25 +1,50 @@
-import { Either, left } from '../../core/either';
+import { Either, left, right } from '../../core/either';
+import { Garden } from '../garden/garden';
+import { sizeType } from '../garden/size';
+import { Coordinate, coordinateType } from '../shared/coordinate';
+import { InvalidCoordinateError } from '../shared/errors/invalid-coordinate-error';
 import { InvalidHeadingError } from './errors/invalid-heading-error';
 import { Heading } from './heading';
 
-type createRobotDTO = {
-  heading: string;
-};
-
 export class Robot {
   private _heading: Heading;
+  private _position: Coordinate;
+  private _garden: Garden;
 
-  private constructor(heading: Heading) {
+  private constructor(heading: Heading, position: Coordinate, garden: Garden) {
     this._heading = heading;
+    this._position = position;
+    this._garden = garden;
   }
 
-  static create({
-    heading,
-  }: createRobotDTO): Either<InvalidHeadingError, Robot> {
-    const headingOrError = Heading.create(heading);
+  get heading(): string {
+    return this._heading.value;
+  }
+
+  get position(): coordinateType {
+    return this._position.value;
+  }
+
+  get gardenSize(): sizeType {
+    return this._garden.size.value;
+  }
+
+  static create(
+    garden: Garden,
+  ): Either<InvalidHeadingError | InvalidCoordinateError, Robot> {
+    const headingOrError = Heading.create('N');
+    const positionOrError = Coordinate.create(0, 0);
 
     if (headingOrError.isLeft()) {
       return left(headingOrError.value);
     }
+    if (positionOrError.isLeft()) {
+      return left(positionOrError.value);
+    }
+
+    const heading = headingOrError.value;
+    const position = positionOrError.value;
+
+    return right(new Robot(heading, position, garden));
   }
 }
