a9887e059252d007e0a4c59ce3853a6ad632face
João Henrique Moretti Pellissari <pellissari.joao@gmail.com>
2021-07-25 18:57:02 -0300

    test: ensure robot can move and change heading

--- src/domain/robot/robot.spec.ts
+++ src/domain/robot/robot.spec.ts
@@ -66,6 +66,88 @@ describe('Robot Entity', () => {
     expect(robot.movements).toEqual(['N']);
   });
 
+  test('should change robot heading on success', () => {
+    const garden = Garden.create({
+      size: { width: 2, height: 2 },
+      irrigablePatches: [{ x: 0, y: 0 }],
+    }).value as Garden;
+
+    const robotOrError = Robot.create(garden);
+    const robot = robotOrError.isRight() ? robotOrError.value : null;
+
+    const changeHeadingOrError = robot.changeHeading('S');
+
+    expect(robot.heading).toEqual('S');
+    expect(changeHeadingOrError.isRight()).toBeTruthy();
+  });
+
+  test('should not change robot heading if heading is invalid', () => {
+    const garden = Garden.create({
+      size: { width: 2, height: 2 },
+      irrigablePatches: [{ x: 0, y: 0 }],
+    }).value as Garden;
+
+    const robotOrError = Robot.create(garden);
+    const robot = robotOrError.isRight() ? robotOrError.value : null;
+
+    const changeHeadingOrError = robot.changeHeading('invalid_heading');
+
+    expect(robot.heading).toEqual('N');
+    expect(changeHeadingOrError.isLeft()).toBeTruthy();
+  });
+
+  test('should not move robot one position to south if out of bounds', () => {
+    const garden = Garden.create({
+      size: { width: 2, height: 2 },
+      irrigablePatches: [{ x: 0, y: 0 }],
+    }).value as Garden;
+
+    const robotOrError = Robot.create(garden);
+    const robot = robotOrError.isRight() ? robotOrError.value : null;
+
+    robot.changeHeading('S');
+
+    const moveOrError = robot.move();
+
+    expect(robot.heading).toEqual('S');
+    expect(moveOrError.isLeft()).toBeTruthy();
+  });
+
+  test('should not move robot one position to west if out of bounds', () => {
+    const garden = Garden.create({
+      size: { width: 2, height: 2 },
+      irrigablePatches: [{ x: 0, y: 0 }],
+    }).value as Garden;
+
+    const robotOrError = Robot.create(garden);
+    const robot = robotOrError.isRight() ? robotOrError.value : null;
+
+    robot.changeHeading('O');
+
+    const moveOrError = robot.move();
+
+    expect(robot.heading).toEqual('O');
+    expect(moveOrError.isLeft()).toBeTruthy();
+  });
+
+  test('should not move robot one position to east if out of bounds', () => {
+    const garden = Garden.create({
+      size: { width: 2, height: 2 },
+      irrigablePatches: [{ x: 0, y: 0 }],
+    }).value as Garden;
+
+    const robotOrError = Robot.create(garden);
+    const robot = robotOrError.isRight() ? robotOrError.value : null;
+
+    robot.changeHeading('L');
+
+    robot.move();
+    const moveOrError = robot.move();
+
+    expect(robot.heading).toEqual('L');
+    expect(moveOrError.isLeft()).toBeTruthy();
+  });
+
   test('should move robot one position to north on success', () => {
     const garden = Garden.create({
       size: { width: 2, height: 2 },
