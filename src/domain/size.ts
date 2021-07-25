461540b42aa139b72d12f88b120483bad5839acc
João Henrique Moretti Pellissari <pellissari.joao@gmail.com>
2021-07-25 12:38:36 -0300

    feat: ensure size can be created with correct values

---
+++ src/domain/size.ts
@@ -0,0 +1,30 @@
+import { Either, left, right } from '../core/either'
+import { InvalidSizeError } from './errors/invalid-size-error'
+
+type sizeType = {
+  width: number
+  height: number
+}
+
+export class Size {
+  private width: number
+  private height: number
+
+  private constructor (width: number, height: number) {
+    this.width = width
+    this.height = height
+    Object.freeze(this)
+  }
+
+  get value (): sizeType {
+    return { width: this.width, height: this.height }
+  }
+
+  static create (width: number, height: number): Either<InvalidSizeError, Size> {
+    if (width <= 0 || height <= 0) {
+      return left(new InvalidSizeError())
+    }
+
+    return right(new Size(width, height))
+  }
+}
