7442d0bdcc7d1f54754f6cd2dca45940ddce93a3
João Henrique Moretti Pellissari <pellissari.joao@gmail.com>
2021-07-25 18:14:32 -0300

    test: ensure robot can be created with default values

--- src/domain/robot/robot.spec.ts
+++ src/domain/robot/robot.spec.ts
@@ -1,18 +1,51 @@
-import { left } from '../../core/either';
-import { InvalidHeadingError } from './errors/invalid-heading-error';
+import { Garden } from '../garden/garden';
 import { Robot } from './robot';
 
-const makeFakeCreateRobotDTO = ({ heading = 'N' }: any): any => ({
-  heading,
-});
-
 describe('Robot Entity', () => {
-  test('should not be possible to create a robot with invalid heading', () => {
-    const robotOrError = Robot.create(
-      makeFakeCreateRobotDTO({ heading: 'invalid_heading' }),
-    );
+  test('should create a robot on success', () => {
+    const garden = Garden.create({
+      size: { width: 2, height: 2 },
+      irrigablePatches: [{ x: 0, y: 0 }],
+    }).value as Garden;
 
-    expect(robotOrError.isLeft()).toBeTruthy();
-    expect(robotOrError).toEqual(left(new InvalidHeadingError()));
+    const robotOrError = Robot.create(garden);
+
+    expect(robotOrError.isRight()).toBeTruthy();
+  });
+
+  test("should return robot's garden size on success", () => {
+    const garden = Garden.create({
+      size: { width: 2, height: 2 },
+      irrigablePatches: [{ x: 0, y: 0 }],
+    }).value as Garden;
+
+    const robotOrError = Robot.create(garden);
+    const robot = robotOrError.isRight() ? robotOrError.value : null;
+
+    expect(robot.gardenSize).toEqual({ width: 2, height: 2 });
+  });
+
+  test("should return robot's position on success", () => {
+    const garden = Garden.create({
+      size: { width: 2, height: 2 },
+      irrigablePatches: [{ x: 0, y: 0 }],
+    }).value as Garden;
+
+    const robotOrError = Robot.create(garden);
+    const robot = robotOrError.isRight() ? robotOrError.value : null;
+
+    expect(robot.position).toEqual({ x: 0, y: 0 });
+  });
+
+  test("should return robot's heading on success", () => {
+    const garden = Garden.create({
+      size: { width: 2, height: 2 },
+      irrigablePatches: [{ x: 0, y: 0 }],
+    }).value as Garden;
+
+    const robotOrError = Robot.create(garden);
+    const robot = robotOrError.isRight() ? robotOrError.value : null;
+
+    expect(robot.heading).toEqual('N');
   });
 });
