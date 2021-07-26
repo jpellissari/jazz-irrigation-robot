0307e92ec94e2bcc100c123391936a00f2773e19
João Henrique Moretti Pellissari <pellissari.joao@gmail.com>
2021-07-26 00:09:16 -0300

    test: should invert robot when heading west with x position smaller than irrigable patch

--- src/use-cases/implementations/resolve-path/resolve-path-use-case.spec.ts
+++ src/use-cases/implementations/resolve-path/resolve-path-use-case.spec.ts
@@ -39,4 +39,24 @@ describe('ResolvePath UseCase', () => {
     expect(response.finalHeading).toEqual(robot.heading);
     expect(response.movements).toEqual(['E']);
   });
+
+  test('should invert robot if robot heading is west and irrigatePatch.x is grater than robot x position', async () => {
+    const garden = Garden.create({
+      size: { width: 4, height: 4 },
+      irrigablePatches: [{ x: 1, y: 3 }],
+    }).value as Garden;
+
+    const robot = Robot.create({
+      garden,
+      initialPosition: { x: 0, y: 1 },
+      initialHeading: 'O',
+    }).value as Robot;
+
+    const sut = new ResolvePathUseCase();
+    const response = await sut.execute(robot);
+
+    expect(response.movements).toEqual(robot.movements);
+    expect(response.finalHeading).toEqual(robot.heading);
+    expect(response.movements).toEqual(['E', 'E']);
+  });
 });
