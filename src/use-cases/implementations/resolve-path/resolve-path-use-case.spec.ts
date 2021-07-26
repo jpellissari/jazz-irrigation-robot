2427557c3d56f0b1d7da70168860f0b4c337a70c
João Henrique Moretti Pellissari <pellissari.joao@gmail.com>
2021-07-26 00:05:36 -0300

    test: should turn robot to left when heading south with x position smaller than irrigable patch

--- src/use-cases/implementations/resolve-path/resolve-path-use-case.spec.ts
+++ src/use-cases/implementations/resolve-path/resolve-path-use-case.spec.ts
@@ -19,4 +19,24 @@ describe('ResolvePath UseCase', () => {
     expect(response.finalHeading).toEqual(robot.heading);
     expect(response.movements).toEqual(['D']);
   });
+
+  test('should move robot to left if robot heading is S and irrigatePatch.x is grater than robot x position', async () => {
+    const garden = Garden.create({
+      size: { width: 4, height: 4 },
+      irrigablePatches: [{ x: 1, y: 3 }],
+    }).value as Garden;
+
+    const robot = Robot.create({
+      garden,
+      initialPosition: { x: 0, y: 1 },
+      initialHeading: 'S',
+    }).value as Robot;
+
+    const sut = new ResolvePathUseCase();
+    const response = await sut.execute(robot);
+
+    expect(response.movements).toEqual(robot.movements);
+    expect(response.finalHeading).toEqual(robot.heading);
+    expect(response.movements).toEqual(['E']);
+  });
 });
