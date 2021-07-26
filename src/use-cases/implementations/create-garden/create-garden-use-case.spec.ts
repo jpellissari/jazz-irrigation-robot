0cc032641de064ead270c173e9d380502c2de32a
João Henrique Moretti Pellissari <pellissari.joao@gmail.com>
2021-07-25 21:36:32 -0300

    refactor: move sut creation to helper method

--- src/use-cases/implementations/create-garden/create-garden-use-case.spec.ts
+++ src/use-cases/implementations/create-garden/create-garden-use-case.spec.ts
@@ -1,33 +1,52 @@
 import { createGardenDTO } from '../../../domain/garden/garden';
 import { ICreateGardenRepository } from '../../../repositories/create-garden';
+import { ICreateGardenUseCase } from '../../protocols/create-garden-use-case';
 import { CreateGardenUseCase } from './create-garden-use-case';
 
+const makeFakeCreateGardenRepository = (): ICreateGardenRepository => {
+  class CreateGardenRepositoryStub implements ICreateGardenRepository {
+    async create(gardenData: createGardenDTO): Promise<void> {
+      return new Promise(resolve => resolve());
+    }
+  }
+
+  return new CreateGardenRepositoryStub();
+};
+
+const makeFakeCreateGardenDTO = (): createGardenDTO => ({
+  size: {
+    width: 3,
+    height: 4,
+  },
+  irrigablePatches: [
+    {
+      x: 1,
+      y: 1,
+    },
+  ],
+});
+
+type SutTypes = {
+  createGardenRepositoryStub: ICreateGardenRepository;
+  sut: ICreateGardenUseCase;
+};
+
+const makeSut = (): SutTypes => {
+  const createGardenRepositoryStub = makeFakeCreateGardenRepository();
+  const sut = new CreateGardenUseCase(createGardenRepositoryStub);
+
+  return {
+    createGardenRepositoryStub,
+    sut,
+  };
+};
 describe('CreateGarden UseCase', () => {
   test('should call CreateGardenRespository with correct params', async () => {
-    class CreateGardenRepositoryStub implements ICreateGardenRepository {
-      async create(gardenData: createGardenDTO): Promise<void> {
-        return new Promise(resolve => resolve());
-      }
-    }
-
-    const createGardenRepositoryStub = new CreateGardenRepositoryStub();
-    const sut = new CreateGardenUseCase(createGardenRepositoryStub);
+    const { sut, createGardenRepositoryStub } = makeSut();
     const createSpy = jest.spyOn(createGardenRepositoryStub, 'create');
-    const makeFakeCreateGardenDTO = {
-      size: {
-        width: 3,
-        height: 4,
-      },
-      irrigablePatches: [
-        {
-          x: 1,
-          y: 1,
-        },
-      ],
-    };
 
-    await sut.execute(makeFakeCreateGardenDTO);
+    await sut.execute(makeFakeCreateGardenDTO());
 
-    expect(createSpy).toHaveBeenCalledWith(makeFakeCreateGardenDTO);
+    expect(createSpy).toHaveBeenCalledWith(makeFakeCreateGardenDTO());
   });
 });
