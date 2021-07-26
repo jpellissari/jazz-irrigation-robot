c0830a1fed5d40d7feaccc4697f25cbb39af35cd
João Henrique Moretti Pellissari <pellissari.joao@gmail.com>
2021-07-26 00:18:14 -0300

    refactor: move robot to constructor

--- src/use-cases/implementations/resolve-path/resolve-path-use-case.ts
+++ src/use-cases/implementations/resolve-path/resolve-path-use-case.ts
@@ -5,31 +5,43 @@ import {
 } from '../../protocols/resolve-path-use-case';
 
 export class ResolvePathUseCase implements IResolvePathUseCase {
-  async execute(robot: Robot): Promise<ResolvePathResponse> {
-    const gardenIrrigablePatches = robot.gardenIrrigablePatches;
+  constructor(private robot: Robot) {}
+  async execute(): Promise<ResolvePathResponse> {
+    const gardenIrrigablePatches = this.robot.gardenIrrigablePatches;
 
     for (const irrigablePatch of gardenIrrigablePatches) {
-      if (irrigablePatch.x > robot.position.x) {
-        if (robot.heading === 'N') {
-          robot.turnRight();
-        } else if (robot.heading === 'S') {
-          robot.turnLeft();
-        } else if (robot.heading === 'O') {
-          robot.turnLeft();
-          robot.turnLeft();
-        }
-      } else if (irrigablePatch.x < robot.position.x) {
-        if (robot.heading === 'N') {
-          robot.turnLeft();
-        } else if (robot.heading === 'S') {
-          robot.turnRight();
-        } else if (robot.heading === 'L') {
-          robot.turnLeft();
-          robot.turnLeft();
-        }
+      if (irrigablePatch.x > this.robot.position.x) {
+        this.turnRobotEast();
+      } else if (irrigablePatch.x < this.robot.position.x) {
+        this.turnRobotWest();
       }
     }
 
-    return { movements: robot.movements, finalHeading: robot.heading };
+    return {
+      movements: this.robot.movements,
+      finalHeading: this.robot.heading,
+    };
+  }
+
+  private turnRobotWest(): void {
+    if (this.robot.heading === 'N') {
+      this.robot.turnLeft();
+    } else if (this.robot.heading === 'S') {
+      this.robot.turnRight();
+    } else if (this.robot.heading === 'L') {
+      this.robot.turnLeft();
+      this.robot.turnLeft();
+    }
+  }
+
+  private turnRobotEast(): void {
+    if (this.robot.heading === 'N') {
+      this.robot.turnRight();
+    } else if (this.robot.heading === 'S') {
+      this.robot.turnLeft();
+    } else if (this.robot.heading === 'O') {
+      this.robot.turnLeft();
+      this.robot.turnLeft();
+    }
   }
 }
